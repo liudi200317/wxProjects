d858e98fdfdbf8181fe4ff324668868700ad991c
liudi <liud.bj@saohuobang.com>
2016-11-16 11:00:47 +0800

    选择图片、预览图片、获取图片信息api

--- project01/pages/API/api.js
+++ project01/pages/API/api.js
@@ -1,7 +1,5 @@
 Page({
   data:{
-      show:"",
-     
       apiList:[{
         id:"1",
         name:"网络",
@@ -9,13 +7,13 @@ Page({
           id:"101",
           name:"发起请求",
           desc:"wx.request",
-          url:"#"
+          url:"network/network"
         },
         {
           id:"102",
           name:"上传、下载",
           desc:"wx.uploadFile、wx.downloadFile",
-          url:"#"
+          url:"network/upload/upload"
         }],
         isOpen:false,
         openClass:"no",
@@ -27,56 +25,36 @@ Page({
           id:"201",
           name:"图片",
           desc:"wx.chooseImage",
-          url:"#"
+          url:"media/picture/picture"
         }],
         isOpen:false,
         openClass:"no"
       }
       ]
   },
-
+/**
+ * 列表切换效果
+ */
   showMenu:function(event){
-    
-      // this.setData({apiList:[]});
-      // console.log("3:",event.currentTarget);
 
-      // var animation = wx.createAnimation({
-      //     duration: 1000,
-      //       timingFunction: 'ease',
-      //   })
-
-      var _currentTarget = event.currentTarget.id;
-      var _apiList = this.data.apiList,
-        i = 0,
-        len = _apiList.length;
+      var _currentTarget = event.currentTarget.id,
+          _apiList = this.data.apiList,
+          i = 0,
+          len = _apiList.length;
 
       for(;i<len;i++){
         var _apiItem = _apiList[i];
-        console.log(_currentTarget)
-        console.log(_apiItem.id)
-        console.log(_currentTarget == _apiItem.id)
         if(_currentTarget == _apiItem.id){
           var isOpen = !_apiItem.isOpen;
           var _open = isOpen ? "show" :"";
           _apiItem.openClass = _open;
           _apiItem.isOpen = isOpen;
-
-          if(isOpen){
-            // animation.translateX(0).opacity(1).height("auto").step()
-            
-          }else{
-              // animation.translateX("100px").opacity(0).height("0").step()
-          }
-          //  _apiItem.animationData = animation.export()
-
           break;
-
         }
       }
 
-      // console.log(_apiList);
-
       this.setData({apiList:_apiList});
-
   }
+
+
 })
