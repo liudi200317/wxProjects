9ca0c39043b85c90fb725444a34933bcf6be2c3a
liudi <liud.bj@saohuobang.com>
2016-11-30 16:18:39 +0800

    完善购物车

--- project01/pages/shoppingCart/shoppingCart.js
+++ project01/pages/shoppingCart/shoppingCart.js
@@ -1,5 +1,47 @@
 Page({
   data:{
+    items:[
+      {
+        sid:"1001",
+        shopName:"第一家店铺",
+        proList:[
+          {
+            pUrl:"../../images/demo.jpg",
+            proName:"1000超级好吃的蛋糕",
+            price:"100",
+            num:'20'
+          
+          },
+          {
+            pUrl:"../../images/demo.jpg",
+            proName:"1001超级好吃的蛋糕",
+            price:"100",
+            num:'20'
+          
+          }
+        ]
+      },
+      {
+        sid:"1002",
+        shopName:"第二家店铺",
+        proList:[
+          {
+            pUrl:"../../images/demo.jpg",
+            proName:"2000超级好吃的蛋糕",
+            price:"100",
+            num:'40'
+          
+          },
+          {
+            pUrl:"../../images/demo.jpg",
+            proName:"2001超级好吃的蛋糕",
+            price:"100",
+            num:'40'
+          
+          }
+        ]
+      }
+      ]
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
@@ -15,5 +57,8 @@ Page({
   },
   onUnload:function(){
     // 页面关闭
+  },
+  checkboxChange:function(e){
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
   }
 })
